777d9c0cee1dc31d6efd9a16eaa635d3a2eba32f
sahil <38555487+sahilgohil@users.noreply.github.com>
2023-10-23 19:51:51 +1100

    remove and replace works

--- ArrayString.java
+++ ArrayString.java
@@ -29,7 +29,31 @@ public class ArrayString {
     }
     public static boolean oneAwayReplace(String s1, String s2)
     {
-        return false;
+        HashMap<Character, Integer> s1Map = new HashMap<>();
+
+        for(char c:s1.toCharArray())
+        {
+            s1Map.put(c, s1Map.getOrDefault(c, 1));
+        }
+
+        for(char c:s2.toCharArray())
+        {
+            if(s1Map.containsKey(c))
+            {
+                s1Map.put(c, s1Map.get(c)-1);
+            }
+        }
+
+        int count = 0;
+        for(char c:s1Map.keySet())
+        {
+            if(s1Map.get(c)>0)
+            {
+                count++;
+            }
+        }
+
+        return count == 1;
     }
     public static boolean oneAwayInsert(String s1, String s2)
     {
@@ -46,9 +70,6 @@ public class ArrayString {
             {
                 s1Map.put(c, s1Map.get(c)-1);
             }
-            else{
-                return false;
-            }
         }
 
         for(char c:s1Map.keySet())
@@ -63,10 +84,35 @@ public class ArrayString {
     }
     public static boolean oneAwayRemove(String s1, String s2)
     {
-        return false;
+        HashMap<Character, Integer> s1Map = new HashMap<>();
+
+        for(char c:s1.toCharArray())
+        {
+            s1Map.put(c, s1Map.getOrDefault(c, 0)+1);
+        }
+
+        for(char c:s2.toCharArray())
+        {
+            if(s1Map.containsKey(c))
+            {
+                s1Map.put(c, s1Map.get(c)-1);
+            }
+        }
+
+        for(char c:s1Map.keySet())
+        {
+            if(s1Map.get(c)>0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(oneAway("pale", "ale")); // insertion works
+        // System.out.println(oneAway("pale", "ale")); // insertion works
+        // System.out.println(oneAway("pale", "paled")); removal works
+        // System.out.println(oneAway("pale","pala")); replace works
     }
 }
